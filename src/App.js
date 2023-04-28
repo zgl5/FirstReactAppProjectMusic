e0865f5f3cd9f56d2c73671633525c140cec792d
zgl5 <ozgul.karaman@gmail.com>
2023-04-27 21:20:52 -0400

    third

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import {Container, InputGroup, FormControl, Button, Row, Card  } from 'react-bootstrap'
-import axios from 'axios'
 import {useState, useEffect} from 'react'
 
 
 function App() {
+const [endPoint, setEndPoint]=useState('')
+const [container, SetContainer] = useState([])
+const [finalpoint, setFinalpoint]= useState('')
 
 
- 
-  async function getData() {
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    console.log(result.data[0].album.tracklist);
-    
-  } catch (error) {
-    console.error(error);
-  }
+async function search(){
+  console.log("search" + setEndPoint)
 }
-getData();
-  return (
+
+useEffect(() =>{
+   fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=+${endPoint}`, {
+   "method": 'GET',
+    "headers": {
+   
+    }
+  })
+
+.then(response => {
+ return response.json()
+})
+
+.then(data => {
+  console.log(data)
+  if (data.error) return null;
+  SetContainer(data.data)
+})
+
+.catch(err => {
+  console.error(err)
+})
+}, [finalpoint])
+
+const onChangeHandler = (e) => {
+  setEndPoint(e.target.value);
+};
+
+const submitHandler = (e) => {
+  e.preventDefault();
+  setFinalpoint(endPoint);   // to executive submit button
+};
+
+
+   return (
     <div className="App">
-      
-      <Container>
+<Container>
         <InputGroup className='mp-3' size='lg'>
-          <FormControl
+          <FormControl 
           placeholder='Search'
           type='input'
           onKeyDown={event=>{
@@ -33,9 +59,10 @@ getData();
              (console.log("hello"))
             }
           }}
-        
-          />
-          <Button>
+         onChange={onChangeHandler}
+          /> 
+          
+          <Button onClick={submitHandler}>
             Search
           </Button>
 
@@ -43,21 +70,23 @@ getData();
       </Container>
           <Container>
             <Row className='mx-2 row row-cols-4'>
-
-            
-            <Card>
-              <Card.Img src="#"/>
+            {container.length>0 && container.map((item,i)=>{
+            return (
+               
+              <Card key={i}>
+              <Card.Img src={item.album.cover}/>
               <Card.Body>
-                 <Card.Title>
-               Album Name
-               </Card.Title>
+                 <Card.Title> {item.title}</Card.Title>
               </Card.Body>
-           
-            </Card>
+              <Button onClick={item.artist.preview}>
+            preview
+          </Button>
+                 </Card>
+                )
+                 })}
             </Row>
           </Container>
-
-          
+      
     </div>
   );
 }
